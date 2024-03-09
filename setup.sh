6b9b3438403ecacbbefdf3d107462cb22d1ff7e2
Daniels Hogans <daniel.santos.ap@gmail.com>
2024-03-08 21:02:02 -0300

    testes inicialização

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer 
+mv /usr/local/bin/composer /usr/local/bin/composer.phar
+echo 'alias composer="php -d memory_limit=-1 /usr/local/bin/composer.phar"' >> ~/.bashrc
+
 alias ls='ls --color'
 composer install
 php artisan migrate
-apache2-foreground
