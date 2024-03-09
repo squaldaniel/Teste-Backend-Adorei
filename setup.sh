d657a4207b754d28f73b50b9fa82b74550c8652a
Daniels Hogans <daniel.santos.ap@gmail.com>
2024-03-08 21:19:22 -0300

    env ajust

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer 
-mv /usr/local/bin/composer /usr/local/bin/composer.phar
-echo 'alias composer="php -d memory_limit=-1 /usr/local/bin/composer.phar"' >> ~/.bashrc
+# curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer 
+# mv /usr/local/bin/composer /usr/local/bin/composer.phar
+#  echo 'alias composer="php -d memory_limit=-1 /usr/local/bin/composer.phar"' >> ~/.bashrc
 
 alias ls='ls --color'
+mv .env.example .env
 composer install
 php artisan migrate
